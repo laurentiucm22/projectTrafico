28f3c0eafc77e93196a37bb677b29693396a1ebb
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-01-27 14:42:51 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <HomePage />
     </div>
   );
 }
--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>What is here?</h1>
+    </div>
+  );
 };
 
 export default Header;
